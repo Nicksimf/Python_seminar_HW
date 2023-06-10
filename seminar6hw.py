d004b60dd1ed9917347093fb46f04aa370121a56
Nick <scream.mmcc@mail.ru>
2023-06-10 18:23:26 +0300

    добавил домашнее задание к семинару№6

--- seminar6hw.py
+++ seminar6hw.py
@@ -5,7 +5,7 @@ a1 = int(input())
 d = int(input())
 n = int(input())
 for i in range(n):
-print(a1 + i * d)
+    print(a1 + i * d)
 
 # Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
 
@@ -14,5 +14,5 @@ list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 min_number = int(input())
 max_number = int(input())
 for i in range(len(list_1)):
-if min_number <= list_1[i] <= max_number:
-print(i)
+    if min_number <= list_1[i] <= max_number:
+        print(i)
